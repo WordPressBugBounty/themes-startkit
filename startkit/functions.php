1c38450c088fdfc6e5e2c2bed0887dea308ef90c
Sean Murphy <sean@iamseanmurphy.com>
2025-04-26 11:17:22 +0000

    15.7

--- startkit/functions.php
+++ startkit/functions.php
@@ -5,7 +5,7 @@ function startkit_setup() {
 	/**
 	 * Define Theme Version
 	 */
-	define( 'STARTKIT_THEME_VERSION', '15.6' );
+	define( 'STARTKIT_THEME_VERSION', '15.7' );
 	
 	/*
 	 * Make theme available for translation.
